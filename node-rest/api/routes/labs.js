d31fd46bb14481829fb7ca443443dc2a90b6bab2
Sign Yellow <signyellow1@gmail.com>
2020-03-19 15:44:35 +0200

    Add lab router and body parser

---
+++ node-rest/api/routes/labs.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next)=>{
+    const lab = {
+        name: req.body.mane,
+        status: req.body.status
+    }
+    res.status(200).json({
+        message: 'Get to Labs route'
+    })
+});
+
+router.get('/:labID',(req, res, next)=>{
+    const id = req.params.labID;
+    res.status(200).json({
+        message: 'You passed an labID, good for you.',
+        id: id
+    });
+});
+module.exports = router;
+
