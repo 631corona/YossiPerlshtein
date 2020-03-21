c9b3e89350ad96dcf81aa16b808d1dbfdfdfd421
Sign Yellow <signyellow1@gmail.com>
2020-03-21 21:53:34 +0200

    create app.js with GET demo request

--- node-rest/app.js
+++ node-rest/app.js
@@ -1,29 +1,10 @@
 const express = require('express');
 const app = express();
-const labRoutes = require('./api/routes/labs');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.get("/users", (req, res, next) =>{
+    res.json(["Tom", "Ann", "Michal"]);
+});
 
-// Route
-app.use('/labs', labRoutes);
-
-app.use((req, res, next) =>{
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
+app.listen(3000, ()=> {
+    console.log("Server is running on port 3000")
 })
-
-app.use((error, req, res, next) =>{
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message: error.message
-        }
-    })
-})
-
-module.exports = app;
